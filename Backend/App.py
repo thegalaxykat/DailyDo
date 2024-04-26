f70b76d9ebebef1554a2eb5d0d67ac230c991d7d
Kat Canavan <60857666+thegalaxykat@users.noreply.github.com>
2024-04-26 01:01:00 -0400

    Add routes

---
+++ Backend/App.py
@@ -0,0 +1,52 @@
+from flask import Flask, send_from_directory, request, redirect
+import os
+from Database import TaskDatabase as task_db
+
+app = Flask(__name__, static_folder=None)
+
+BUILD_FOLDER = "React_Front_End/build"
+
+
+@app.route("/")
+def get_root():
+    return redirect("/index.html")
+
+
+@app.route("/<path:p>")
+def serveFile(p):
+    try:
+        return send_from_directory(BUILD_FOLDER, p)
+    except:
+        return ("File not found", 404) # classic
+
+
+@app.route("/get-tasks")
+def get_tasks():
+    with task_db() as db:
+        try:
+            tasks = db.get_tasks()
+            return {"tasks": tasks}
+        except Exception as e:
+            return str(e), 400
+
+
+@app.route("/add-task", methods=["POST"])
+def add_task():
+    body = request.json
+    with task_db() as db:
+        try:
+            db.add_task(body["task"], body["today"])
+            return "", 204 # means successful action with no content
+        except Exception as e:
+            return str(e), 400 # bad request
+
+
+@app.route("/delete-task", methods=["POST"])
+def delete_task():
+    body = request.json
+    with task_db() as db:
+        try:
+            db.delete_task(body["id"])
+            return "", 204
+        except Exception as e:
+            return str(e), 400
